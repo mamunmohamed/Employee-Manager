b7df355deb5b4572dc97b030401fe9f54b435f7e
Mamun Mohamed <mohamedmamun85@gmail.com>
2021-03-13 16:19:04 +0100

    Project Upload

---
+++ src/main/java/com/mamunmohamed/EmployeeManager/model/Employee.java
@@ -0,0 +1,11 @@
+package com.mamunmohamed.EmployeeManager.model;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+
+}
