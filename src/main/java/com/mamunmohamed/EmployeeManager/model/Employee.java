06573051653879fc8a472b6c4967211890f2268c
Mamun Mohamed <mohamedmamun85@gmail.com>
2021-03-13 16:27:51 +0100

    Setup primary key and generation

--- src/main/java/com/mamunmohamed/EmployeeManager/model/Employee.java
+++ src/main/java/com/mamunmohamed/EmployeeManager/model/Employee.java
@@ -1,10 +1,18 @@
 package com.mamunmohamed.EmployeeManager.model;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity
 public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
